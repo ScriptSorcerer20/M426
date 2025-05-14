6e071c4b19fae3f14d7d504f55eb4fe880e7531f
IM23a-spirgif <spirgif@bzz.ch>
2025-05-14 14:20:03 +0200

    Update template routing and static file paths

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def load_user(user_id):
 # Home route
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("index.html")
 
 # Register route
 @app.route('/register', methods=["GET", "POST"])
